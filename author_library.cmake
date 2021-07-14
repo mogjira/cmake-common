20d4d986bbf285dd862fa090bb1b017045cf9523
Michael <mokchira@gmail.com>
2021-07-14 13:14:39 -0400

    ud

--- author_library.cmake
+++ author_library.cmake
@@ -19,7 +19,7 @@ function(author_library lib_name)
         $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>)
 
     if(L_EXTRA_INC_DIRS)
-        target_include_directories(${lib_name} PRIVATE 
+        target_include_directories(${lib_name} PUBLIC
             $<BUILD_INTERFACE:${L_EXTRA_INC_DIRS}>)
     endif()
 
